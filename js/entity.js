dba067af05b409d414bd53d3386087e263e3c5d5
Richard Schneider <richard@schneiderbox.net>
2015-08-22 07:13:56 -0500

    add entity object (and give turtle a tail)

---
+++ js/entity.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function Entity(options){
+	this.pos = options.pos || {x: 0, y: 0};
+	this.velocity = options.velocity || {x: 0, y:0};
+
+	this.image = options.image;
+	this.flipImage = options.flipImage;
+}
+
+Entity.prototype.update = function(delta){
+	this.velocity.y += GRAVITY * delta/1000;
+
+	// Land on bricks
+	var footTile = {
+		x: Math.floor(this.pos.x / GAME_TILE_SIZE),
+		y: Math.floor((this.pos.y + this.image.height/2) / GAME_TILE_SIZE)
+	}
+	if(world.bricks[footTile.x][footTile.y]){
+		this.pos.y = footTile.y*GAME_TILE_SIZE - this.image.height/2;
+		if(this.velocity.y > 0){
+			this.velocity.y = 0;
+		}
+	}
+
+	this.pos.x += this.velocity.x * delta/1000;
+	this.pos.y += this.velocity.y * delta/1000;
+}
--- js/turtle.js
+++ js/turtle.js
@@ -5,41 +5,27 @@ var IMAGE_TURTLE = document.getElementById('IMAGE_TURTLE');
 var TURTLE_SPEED = 100;
 
 function Turtle(){
-	this.pos = {
-		x: 140,
-		y: 0
-	}
-
-	this.velocity = {
-		x: 0,
-		y: 0
-	}
-
 	this.image = IMAGE_TURTLE;
-
-	this.onGround = false;
+	this.flipImage = false;
+	Entity.call(this, {
+		pos: {x: 140, y: 0},
+		image: IMAGE_TURTLE
+	});
 }
 
-Turtle.prototype.update = function(delta){
-	this.velocity.y += GRAVITY*delta/1000;
-	
-	// Land on bricks
-	var footTile = {
-		x: Math.floor(this.pos.x / GAME_TILE_SIZE),
-		y: Math.floor((this.pos.y + this.image.height/2) / GAME_TILE_SIZE)
-	}
-	if(world.bricks[footTile.x][footTile.y]){
-		this.pos.y = footTile.y*GAME_TILE_SIZE - this.image.height/2;
-		this.velocity.y = 0;
-	}
+Turtle.prototype = Object.create(Entity);
+Turtle.prototype.constructor = Turtle;
 
+Turtle.prototype.update = function(delta){
 	// Move left and right
 	this.velocity.x = 0;
 	if(controller.buttons.left){
 		this.velocity.x = -TURTLE_SPEED;
+		this.flipImage = false;
 	}
 	if(controller.buttons.right){
 		this.velocity.x = TURTLE_SPEED;
+		this.flipImage = true;
 	}
 
 	// Jump
@@ -50,8 +36,8 @@ Turtle.prototype.update = function(delta){
 		}
 	}
 
-	this.pos.x += this.velocity.x * delta/1000;
-	this.pos.y += this.velocity.y * delta/1000;
+	Entity.prototype.update.call(this, delta);
+
 	if(this.pos.x - this.image.width/2 < 0){
 		this.pos.x = this.image.width/2
 	}
